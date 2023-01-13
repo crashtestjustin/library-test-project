db4acd38b54a6875484477560adb8853588ff5a2
Justin Elliott <thecrashtestjustin@gmail.com>
2023-01-13 07:30:11 -0800

    object conttructor updates
    added a book contructor and function to push new books to the myLibrary
    array

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
+//object contructor
+
 let myLibrary = [];
 
-function bookConstructor() {
-  //create new books here
+function Book(title, author, pages, read) {
+  //create new books attributes
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLib() {
-  //call to add books to library here
+function addBookToLib(t, a, p, r) {
+  //call to add books to library
+  let book = new Book(t, a, p, r);
+  console.log(book);
+  myLibrary.push(book);
 }
+
+addBookToLib("test", "book", 900, true);
+addBookToLib("test", "book2", 9000, false);
+console.log(myLibrary);
+
+//User Interface Actions
 
 const addNewBook = document.querySelector(".add-new");
+const formModal = document.querySelector(".new-book-modal");
+const modalOverlay = document.querySelector(".overlay");
 
 addNewBook.addEventListener("click", (e) => {
-  const formModal = document.querySelector(".new-book-modal");
-  const modalOverlay = document.querySelector(".overlay");
   formModal.classList.remove("hidden-modal");
   modalOverlay.classList.add("active");
 });
+
+const overlay = document.getElementById("overlay");
+
+overlay.addEventListener("click", (e) => {
+  overlay.onclick = closeModal();
+});
+
+function closeModal() {
+  formModal.classList.add("hidden-modal");
+  modalOverlay.classList.remove("active");
+}
 
 function testPrint() {
   console.log("test");
